be4e2a0a2d1e2d972bb8ec0253623b507d415721
Muchlish Choeruddin <muchlishchoeruddin96@gmail.com>
2019-05-21 17:01:32 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,20 +8,50 @@ $('.grid').isotope({
   }
 });
 $('.filter a').click(function(){
-	$('grid-item:not('+$(this).attr("data-filter")+')').addClass('productdisable');
+	// alert('asd');
+	// alert()
+	var filterval = $(this).attr("data-filter");
+	$('.grid').isotope({ filter: filterval });
+	$('.filter a').removeClass('active');
+	$(this).addClass('active');
 });
+if ($(window).scrollTop()>=60) {
+	$('.navbar').css({
+		'background-color':'white',
+	});
+	$('.navpage .navbar').css({
+		'top':'0',
+		'padding-top':'1em',
+		'padding-bottom':'1em',
+	});
+}else{
+	$('.navbar').css({
+		'background-color':'transparent',
+	});
+	$('.navpage .navbar').css({
+		'top':'4em',
+		'padding-top':'2em',
+		'padding-bottom':'2em',
+	});
+}
 $(window).scroll(function(){
 	if ($(this).scrollTop()>=60) {
 		$('.navbar').css({
 			'background-color':'white',
 		});
 		$('.navpage .navbar').css({
-			'top':'0'
+			'top':'0',
+			'padding-top':'1em',
+			'padding-bottom':'1em',
 		});
 	}else{
 		$('.navbar').css({
 			'background-color':'transparent',
 		});
-		$('.navpage .navbar').css('top','4em');
+		$('.navpage .navbar').css({
+			'top':'4em',
+			'padding-top':'2em',
+			'padding-bottom':'2em',
+		});
 	}
 });
